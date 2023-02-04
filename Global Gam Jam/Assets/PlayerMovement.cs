9f2c4803ce5320a8780f51d09702939681cd63c8
riccardo-torlaini <riccardotorlainiofficial@gmail.com>
2023-02-04 15:27:09 +0100

    transformation

--- Global Gam Jam/Assets/PlayerMovement.cs
+++ Global Gam Jam/Assets/PlayerMovement.cs
@@ -6,17 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private float Move;
-
+    public Sprite spriteA;
+    public Sprite spriteB;
     public float jump;
-
+    public int transformation;
     public bool isJumping;
-
+    public int treshold;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        transformation = 0;
+        speed = 5;
+        treshold = 20;
     }
+
 
     // Update is called once per frame
     void Update()
@@ -24,11 +29,30 @@ public class PlayerMovement : MonoBehaviour
         Move = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(speed * Move, rb.velocity.y);
-
+        while( speed >= treshold ){
+            speed = speed - speed * 0.125f;
+        }
         if(Input.GetButtonDown("Jump") && isJumping == false)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
             Debug.Log("jump");
+        }
+        if(Input.GetKeyDown(KeyCode.L)){
+            if(GetComponent<SpriteRenderer>().sprite != spriteA){
+                transformation = transformation + 1;
+                speed = 0.9f*speed;
+            }
+            GetComponent<SpriteRenderer>().sprite = spriteA;
+
+        }
+        if(Input.GetKeyDown(KeyCode.K)){
+            if(GetComponent<SpriteRenderer>().sprite != spriteB){
+                transformation = transformation + 1;
+                speed = 1.9f*speed;
+
+               }
+            GetComponent<SpriteRenderer>().sprite = spriteB;
+
         }
     }
 
